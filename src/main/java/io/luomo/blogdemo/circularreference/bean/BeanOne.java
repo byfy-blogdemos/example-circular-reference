ed2186e6cb6535037753c5f95a1e17b2d7644b9d
hatakawas <hatakawas@163.com>
2019-07-30 12:05:47 +0800

    update

--- src/main/java/io/luomo/blogdemo/circularreference/bean/BeanOne.java
+++ src/main/java/io/luomo/blogdemo/circularreference/bean/BeanOne.java
@@ -14,20 +14,4 @@ public class BeanOne {
     public BeanOne(BeanTwo beanTwo) {
         this.beanTwo = beanTwo;
     }
-
-    public void sayHi() {
-        String message = String.format("Hi, this is %s.", this);
-        System.out.println(message);
-    }
-
-    public void talk() {
-        beanTwo.sayHi();
-        String reply = String.format("Hello, %s, glad to see you.", beanTwo);
-        System.out.println(reply);
-    }
-
-    @Override
-    public String toString() {
-        return "beanOne";
-    }
 }
